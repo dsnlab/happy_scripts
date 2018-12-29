695e4b3fb63a0119eebed9795b7f42bb06ca8d43
Dani Cosme <dani.cosme@gmail.com>
2018-12-28 18:07:19 -0800

    added t tests

---
+++ fMRI/rx/social_self_change.m
@@ -0,0 +1,133 @@
+%-----------------------------------------------------------------------
+% Job saved on 28-Dec-2018 17:56:42 by cfg_util (rev $Rev: 6942 $)
+% spm SPM - SPM12 (7219)
+% cfg_basicio BasicIO - Unknown
+%-----------------------------------------------------------------------
+matlabbatch{1}.spm.stats.factorial_design.dir = {'/projects/dsnlab/shared/FP/nonbids_data/fMRI/rx/svc/wave1/social_self_change'};
+%%
+matlabbatch{1}.spm.stats.factorial_design.des.t1.scans = {
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP001/con_0015.nii'
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP002/con_0015.nii'
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP003/con_0015.nii'
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP004/con_0015.nii'
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP005/con_0015.nii'
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP006/con_0015.nii'
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP007/con_0015.nii'
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP008/con_0015.nii'
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP009/con_0015.nii'
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP010/con_0015.nii'
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP011/con_0015.nii'
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP012/con_0015.nii'
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP013/con_0015.nii'
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP014/con_0015.nii'
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP015/con_0015.nii'
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP016/con_0015.nii'
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP017/con_0015.nii'
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP018/con_0015.nii'
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP019/con_0015.nii'
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP020/con_0015.nii'
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP022/con_0015.nii'
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP023/con_0015.nii'
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP024/con_0015.nii'
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP025/con_0015.nii'
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP026/con_0015.nii'
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP027/con_0015.nii'
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP028/con_0015.nii'
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP029/con_0015.nii'
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP030/con_0015.nii'
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP031/con_0015.nii'
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP032/con_0015.nii'
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP033/con_0015.nii'
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP034/con_0015.nii'
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP035/con_0015.nii'
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP036/con_0015.nii'
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP037/con_0015.nii'
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP038/con_0015.nii'
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP039/con_0015.nii'
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP040/con_0015.nii'
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP041/con_0015.nii'
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP042/con_0015.nii'
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP043/con_0015.nii'
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP044/con_0015.nii'
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP045/con_0015.nii'
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP046/con_0015.nii'
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP047/con_0015.nii'
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP048/con_0015.nii'
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP050/con_0015.nii'
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP051/con_0015.nii'
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP052/con_0015.nii'
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP053/con_0015.nii'
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP054/con_0015.nii'
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP055/con_0015.nii'
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP056/con_0015.nii'
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP058/con_0015.nii'
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP059/con_0015.nii'
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP060/con_0015.nii'
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP061/con_0015.nii'
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP062/con_0015.nii'
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP063/con_0015.nii'
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP064/con_0015.nii'
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP065/con_0015.nii'
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP066/con_0015.nii'
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP068/con_0015.nii'
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP069/con_0015.nii'
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP070/con_0015.nii'
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP071/con_0015.nii'
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP072/con_0015.nii'
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP074/con_0015.nii'
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP075/con_0015.nii'
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP076/con_0015.nii'
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP077/con_0015.nii'
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP078/con_0015.nii'
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP079/con_0015.nii'
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP081/con_0015.nii'
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP084/con_0015.nii'
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP086/con_0015.nii'
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP087/con_0015.nii'
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP088/con_0015.nii'
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP089/con_0015.nii'
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP090/con_0015.nii'
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP092/con_0015.nii'
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP093/con_0015.nii'
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP094/con_0015.nii'
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP095/con_0015.nii'
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP096/con_0015.nii'
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP097/con_0015.nii'
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP098/con_0015.nii'
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP100/con_0015.nii'
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP101/con_0015.nii'
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP102/con_0015.nii'
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP103/con_0015.nii'
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP104/con_0015.nii'
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP106/con_0015.nii'
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP107/con_0015.nii'
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP109/con_0015.nii'
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP110/con_0015.nii'
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP111/con_0015.nii'
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP112/con_0015.nii'
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP113/con_0015.nii'
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP115/con_0015.nii'
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP119/con_0015.nii'
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP120/con_0015.nii'
+                                                          '/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/event/sub-FP122/con_0015.nii'
+                                                          };
+%%
+matlabbatch{1}.spm.stats.factorial_design.cov = struct('c', {}, 'cname', {}, 'iCFI', {}, 'iCC', {});
+matlabbatch{1}.spm.stats.factorial_design.multi_cov = struct('files', {}, 'iCFI', {}, 'iCC', {});
+matlabbatch{1}.spm.stats.factorial_design.masking.tm.tm_none = 1;
+matlabbatch{1}.spm.stats.factorial_design.masking.im = 0;
+matlabbatch{1}.spm.stats.factorial_design.masking.em = {'/gpfs/projects/dsnlab/shared/SPM12/canonical/MNI152lin_T1_2mm_brain_mask.nii,1'};
+matlabbatch{1}.spm.stats.factorial_design.globalc.g_omit = 1;
+matlabbatch{1}.spm.stats.factorial_design.globalm.gmsca.gmsca_no = 1;
+matlabbatch{1}.spm.stats.factorial_design.globalm.glonorm = 1;
+matlabbatch{2}.spm.stats.fmri_est.spmmat(1) = cfg_dep('Factorial design specification: SPM.mat File', substruct('.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','spmmat'));
+matlabbatch{2}.spm.stats.fmri_est.write_residuals = 0;
+matlabbatch{2}.spm.stats.fmri_est.method.Classical = 1;
+matlabbatch{3}.spm.stats.con.spmmat(1) = cfg_dep('Model estimation: SPM.mat File', substruct('.','val', '{}',{2}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','spmmat'));
+matlabbatch{3}.spm.stats.con.consess{1}.tcon.name = 'social self > change';
+matlabbatch{3}.spm.stats.con.consess{1}.tcon.weights = 1;
+matlabbatch{3}.spm.stats.con.consess{1}.tcon.sessrep = 'none';
+matlabbatch{3}.spm.stats.con.consess{2}.tcon.name = 'social change > self';
+matlabbatch{3}.spm.stats.con.consess{2}.tcon.weights = -1;
+matlabbatch{3}.spm.stats.con.consess{2}.tcon.sessrep = 'none';
+matlabbatch{3}.spm.stats.con.delete = 1;
