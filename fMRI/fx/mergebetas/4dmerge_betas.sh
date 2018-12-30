e3cd4eadf287504ce4c181b86d05179904379db0
Dani Cosme <dani.cosme@gmail.com>
2018-12-29 18:49:47 -0800

    added scripts to merge betas

---
+++ fMRI/fx/mergebetas/4dmerge_betas.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# This script uses fsl's fslmerge function
+# to concatenante all the volumes from the
+# participant's resting state scan.
+
+
+module load fsl
+
+echo "Running ${SUB}"
+
+# Set path and cd into it
+SUBDIR=/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/betaseries/sub-"${SUB}"
+cd $SUBDIR
+
+# Merge betas
+echo "Merging betas"
+BETAS=`cat /projects/dsnlab/shared/FP/FP_scripts/fMRI/fx/mergebetas/subject_beta_info/self_${SUB}.txt`
+
+fslmerge -t betas.nii.gz ${BETAS[@]}
+echo "Gunzipping betas"
+gunzip self_betas.nii.gz
