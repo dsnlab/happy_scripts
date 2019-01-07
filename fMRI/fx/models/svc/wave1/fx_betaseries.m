94ef4a6b8ff9a9b0389c5cbc67885e03a971f01b
Dani Cosme <dani.cosme@gmail.com>
2019-01-07 14:12:47 -0800

    fixed errors

--- fMRI/fx/models/svc/wave1/fx_betaseries.m
+++ fMRI/fx/models/svc/wave1/fx_betaseries.m
@@ -1,36 +1,39 @@
 %-----------------------------------------------------------------------
-% Job saved on 29-Dec-2018 14:33:22 by cfg_util (rev $Rev: 6942 $)
+% Job saved on 07-Jan-2019 14:12:17 by cfg_util (rev $Rev: 6942 $)
 % spm SPM - SPM12 (7219)
 % cfg_basicio BasicIO - Unknown
 %-----------------------------------------------------------------------
-matlabbatch{1}.spm.util.exp_frames.files = {'/projects/dsnlab/shared/FP/bids_data/derivatives/fmriprep/sub-FP001/ses-wave1/func/s2_sub-FP001_ses-wave1_task-SVC_acq-01_bold_space-MNI152NLin2009cAsym_preproc.nii,1'};
+matlabbatch{1}.spm.util.exp_frames.files = {'/projects/dsnlab/shared/FP/bids_data/derivatives/fmriprep/sub-FP001/ses-wave1/func/s2_sub-FP001_ses-wave1_task-SVC_acq-01_bold_space-T1w_preproc.nii'};
 matlabbatch{1}.spm.util.exp_frames.frames = Inf;
-matlabbatch{2}.spm.util.exp_frames.files = {'/projects/dsnlab/shared/FP/bids_data/derivatives/fmriprep/sub-FP001/ses-wave1/func/s2_sub-FP001_ses-wave1_task-SVC_acq-02_bold_space-MNI152NLin2009cAsym_preproc.nii,1'};
+matlabbatch{2}.spm.util.exp_frames.files = {'/projects/dsnlab/shared/FP/bids_data/derivatives/fmriprep/sub-FP001/ses-wave1/func/s2_sub-FP001_ses-wave1_task-SVC_acq-02_bold_space-T1w_preproc.nii'};
 matlabbatch{2}.spm.util.exp_frames.frames = Inf;
-matlabbatch{3}.spm.stats.fmri_spec.dir = {'/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/betaseries/sub-FP001'};
-matlabbatch{3}.spm.stats.fmri_spec.timing.units = 'secs';
-matlabbatch{3}.spm.stats.fmri_spec.timing.RT = 2;
-matlabbatch{3}.spm.stats.fmri_spec.timing.fmri_t = 72;
-matlabbatch{3}.spm.stats.fmri_spec.timing.fmri_t0 = 36;
-matlabbatch{3}.spm.stats.fmri_spec.sess(1).scans(1) = cfg_dep('Expand image frames: Expanded filename list.', substruct('.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','files'));
-matlabbatch{3}.spm.stats.fmri_spec.sess(1).cond = struct('name', {}, 'onset', {}, 'duration', {}, 'tmod', {}, 'pmod', {}, 'orth', {});
-matlabbatch{3}.spm.stats.fmri_spec.sess(1).multi = {'/projects/dsnlab/shared/FP/FP_scripts/fMRI/fx/multiconds/svc/wave1/betaseries/FP001_wave1_SVC1.mat'};
-matlabbatch{3}.spm.stats.fmri_spec.sess(1).regress = struct('name', {}, 'val', {});
-matlabbatch{3}.spm.stats.fmri_spec.sess(1).multi_reg = {'/projects/dsnlab/shared/FP/FP_scripts/fMRI/fx/motion/auto-motion-fmriprep/rp_txt/rp_FP001_1_SVC_1.txt'};
-matlabbatch{3}.spm.stats.fmri_spec.sess(1).hpf = 100;
-matlabbatch{3}.spm.stats.fmri_spec.sess(2).scans(1) = cfg_dep('Expand image frames: Expanded filename list.', substruct('.','val', '{}',{2}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','files'));
-matlabbatch{3}.spm.stats.fmri_spec.sess(2).cond = struct('name', {}, 'onset', {}, 'duration', {}, 'tmod', {}, 'pmod', {}, 'orth', {});
-matlabbatch{3}.spm.stats.fmri_spec.sess(2).multi = {'/projects/dsnlab/shared/FP/FP_scripts/fMRI/fx/multiconds/svc/wave1/betaseries/FP001_wave1_SVC2.mat'};
-matlabbatch{3}.spm.stats.fmri_spec.sess(2).regress = struct('name', {}, 'val', {});
-matlabbatch{3}.spm.stats.fmri_spec.sess(2).multi_reg = {'/projects/dsnlab/shared/FP/FP_scripts/fMRI/fx/motion/auto-motion-fmriprep/rp_txt/rp_FP001_1_SVC_2.txt'};
-matlabbatch{3}.spm.stats.fmri_spec.sess(2).hpf = 100;
-matlabbatch{3}.spm.stats.fmri_spec.fact = struct('name', {}, 'levels', {});
-matlabbatch{3}.spm.stats.fmri_spec.bases.hrf.derivs = [0 0];
-matlabbatch{3}.spm.stats.fmri_spec.volt = 1;
-matlabbatch{3}.spm.stats.fmri_spec.global = 'None';
-matlabbatch{3}.spm.stats.fmri_spec.mthresh = -Inf;
-matlabbatch{3}.spm.stats.fmri_spec.mask = {'/projects/dsnlab/shared/SPM12/canonical/MNI152lin_T1_2mm_brain_mask.nii,1'};
-matlabbatch{3}.spm.stats.fmri_spec.cvi = 'FAST';
-matlabbatch{4}.spm.stats.fmri_est.spmmat(1) = cfg_dep('fMRI model specification: SPM.mat File', substruct('.','val', '{}',{3}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','spmmat'));
-matlabbatch{4}.spm.stats.fmri_est.write_residuals = 0;
-matlabbatch{4}.spm.stats.fmri_est.method.Classical = 1;
+matlabbatch{3}.cfg_basicio.file_dir.file_ops.cfg_gunzip_files.files = {'/projects/dsnlab/shared/FP/bids_data/derivatives/fmriprep/sub-FP001/ses-wave1/func/sub-FP001_ses-wave1_task-SVC_acq-01_bold_space-T1w_brainmask.nii.gz'};
+matlabbatch{3}.cfg_basicio.file_dir.file_ops.cfg_gunzip_files.outdir = {''};
+matlabbatch{3}.cfg_basicio.file_dir.file_ops.cfg_gunzip_files.keep = true;
+matlabbatch{4}.spm.stats.fmri_spec.dir = {'/projects/dsnlab/shared/FP/nonbids_data/fMRI/fx/models/svc/wave1/betaseries/sub-FP001'};
+matlabbatch{4}.spm.stats.fmri_spec.timing.units = 'secs';
+matlabbatch{4}.spm.stats.fmri_spec.timing.RT = 2;
+matlabbatch{4}.spm.stats.fmri_spec.timing.fmri_t = 72;
+matlabbatch{4}.spm.stats.fmri_spec.timing.fmri_t0 = 36;
+matlabbatch{4}.spm.stats.fmri_spec.sess(1).scans(1) = cfg_dep('Expand image frames: Expanded filename list.', substruct('.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','files'));
+matlabbatch{4}.spm.stats.fmri_spec.sess(1).cond = struct('name', {}, 'onset', {}, 'duration', {}, 'tmod', {}, 'pmod', {}, 'orth', {});
+matlabbatch{4}.spm.stats.fmri_spec.sess(1).multi = {'/projects/dsnlab/shared/FP/FP_scripts/fMRI/fx/multiconds/svc/wave1/betaseries/FP001_wave1_SVC1.mat'};
+matlabbatch{4}.spm.stats.fmri_spec.sess(1).regress = struct('name', {}, 'val', {});
+matlabbatch{4}.spm.stats.fmri_spec.sess(1).multi_reg = {'/projects/dsnlab/shared/FP/FP_scripts/fMRI/fx/motion/auto-motion-fmriprep/rp_txt/rp_FP001_1_SVC_1.txt'};
+matlabbatch{4}.spm.stats.fmri_spec.sess(1).hpf = 100;
+matlabbatch{4}.spm.stats.fmri_spec.sess(2).scans(1) = cfg_dep('Expand image frames: Expanded filename list.', substruct('.','val', '{}',{2}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','files'));
+matlabbatch{4}.spm.stats.fmri_spec.sess(2).cond = struct('name', {}, 'onset', {}, 'duration', {}, 'tmod', {}, 'pmod', {}, 'orth', {});
+matlabbatch{4}.spm.stats.fmri_spec.sess(2).multi = {'/projects/dsnlab/shared/FP/FP_scripts/fMRI/fx/multiconds/svc/wave1/betaseries/FP001_wave1_SVC2.mat'};
+matlabbatch{4}.spm.stats.fmri_spec.sess(2).regress = struct('name', {}, 'val', {});
+matlabbatch{4}.spm.stats.fmri_spec.sess(2).multi_reg = {'/projects/dsnlab/shared/FP/FP_scripts/fMRI/fx/motion/auto-motion-fmriprep/rp_txt/rp_FP001_1_SVC_2.txt'};
+matlabbatch{4}.spm.stats.fmri_spec.sess(2).hpf = 100;
+matlabbatch{4}.spm.stats.fmri_spec.fact = struct('name', {}, 'levels', {});
+matlabbatch{4}.spm.stats.fmri_spec.bases.hrf.derivs = [0 0];
+matlabbatch{4}.spm.stats.fmri_spec.volt = 1;
+matlabbatch{4}.spm.stats.fmri_spec.global = 'None';
+matlabbatch{4}.spm.stats.fmri_spec.mthresh = -Inf;
+matlabbatch{4}.spm.stats.fmri_spec.mask(1) = cfg_dep('Gunzip Files: Gunzipped Files', substruct('.','val', '{}',{3}, '.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('()',{':'}));
+matlabbatch{4}.spm.stats.fmri_spec.cvi = 'FAST';
+matlabbatch{5}.spm.stats.fmri_est.spmmat(1) = cfg_dep('fMRI model specification: SPM.mat File', substruct('.','val', '{}',{4}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','spmmat'));
+matlabbatch{5}.spm.stats.fmri_est.write_residuals = 0;
+matlabbatch{5}.spm.stats.fmri_est.method.Classical = 1;
